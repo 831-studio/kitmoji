c075447df6a9626d23c94c6162b13f9357728f30
General <general@Mac.lan>
2025-08-05 23:51:54 -0400

    Create Vercel serverless function for sitemap.xml

---
+++ api/sitemap.xml.js
@@ -0,0 +1,39 @@
+// Vercel serverless function for sitemap
+export default function handler(req, res) {
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://www.kitmoji.net/</loc>
+    <lastmod>2025-08-06T03:36:00.000Z</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
+  <url>
+    <loc>https://www.kitmoji.net/unicode</loc>
+    <lastmod>2025-08-06T03:36:00.000Z</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.kitmoji.net/all-emojis</loc>
+    <lastmod>2025-08-06T03:36:00.000Z</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.kitmoji.net/popular-emojis</loc>
+    <lastmod>2025-08-06T03:36:00.000Z</lastmod>
+    <changefreq>weekly</changefreq>
+    <priority>0.9</priority>
+  </url>
+  <url>
+    <loc>https://www.kitmoji.net/new-emojis</loc>
+    <lastmod>2025-08-06T03:36:00.000Z</lastmod>
+    <changefreq>monthly</changefreq>
+    <priority>0.8</priority>
+  </url>
+</urlset>`;
+
+  res.setHeader('Content-Type', 'application/xml; charset=utf-8');
+  res.status(200).send(sitemap);
+}
